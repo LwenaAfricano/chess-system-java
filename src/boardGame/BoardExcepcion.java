f96f97f1e966d327db29104008399177f87a2413
Lwena Africano <lwliana1@gmail.com>
2024-09-19 11:43:04 +0100

    BoardException and defensive programming

---
+++ src/boardGame/BoardExcepcion.java
@@ -0,0 +1,15 @@
+package boardGame;
+
+public class BoardExcepcion extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BoardExcepcion(String msg) {
+		super(msg);
+	}
+	
+	
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,9 +28,10 @@ public class ChessMatch {
 	
 	//iniciar a partida de chess colocando as peças no taboleiro
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(3, 2));
+		board.placePiece(new King(board, Color.BLACK), new Position(1, 4));
 		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		
 
 	}
 	
